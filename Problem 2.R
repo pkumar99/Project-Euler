8479b2428afcc8e6e18a8dd5b7bc8c04594aaf9c
Guest User <Guest@us-physics-cart-01.local>
2016-10-20 08:58:23 -0400

    Working through problem 2

--- Problem 2.R
+++ Problem 2.R
@@ -1,5 +1,8 @@
-Fibonnaci <- function(x) {
+#This function will print the terms of the Fibonacci sequence that are less than x
+Fibonacci <- function(x) {
   fib <- c(1,2);
-  fib <- c(fib,fib[length(fib)-1]+fib[length(fib)])
-  return(fib[length(fib)])
+  while(fib[length(fib)-1]+fib[length(fib)] < x) {
+    fib <- c(fib,fib[length(fib)-1]+fib[length(fib)])
+  }
+  return(fib)
 }
